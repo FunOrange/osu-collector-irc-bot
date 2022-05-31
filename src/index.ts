31f2098b01ff0ffc4f5cebacfd68afcee823b4c7
FunOrange <junarvi@yahoo.ca>
2022-05-31 14:57:32 -0400

    bot works!!!

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv'
+import * as irc from 'irc'
 dotenv.config()
 
-const ircUsername = process.env.IRC_USERNAME
-const ircPassword = process.env.IRC_SERVER_PASSWORD
-console.log('username', ircUsername)
-console.log('password', ircPassword)
+const bot = new irc.Client('irc.ppy.sh', process.env.IRC_USERNAME, {
+  channels: [],
+  userName: process.env.IRC_USERNAME,
+  password: process.env.IRC_PASSWORD,
+  port: parseInt(process.env.IRC_PORT),
+})
+
+bot.addListener('message', function (from, to, text, message) {
+  console.log('from', from)
+  console.log('to', to)
+  console.log('text', text)
+  console.log('message', message)
+  bot.say(from, 'hello! ' + text)
+})
